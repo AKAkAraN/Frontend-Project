44e54c9a8e6ecdbb3de142668f1eb0681fd36056
AKAkAraN <69759152+AKAkAraN@users.noreply.github.com>
2021-03-27 14:20:46 +0530

    Part A

---
+++ script/index.js
@@ -0,0 +1,14 @@
+function showCreatePost(){
+    var modal = document.getElementById("create-post-modal");
+    modal.style.display = "flex";
+  }
+  
+  function hideCreatePostModal(){
+    var modal = document.getElementById("create-post-modal");
+    modal.style.display = "none";
+  }
+  
+  function navigateToBlogList(){
+    location.href = "html/bloglist.html"
+  }
+  
